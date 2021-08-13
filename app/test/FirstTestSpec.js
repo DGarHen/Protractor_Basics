2725057e0fa634a02cb687a11df286e97d31c38c
Diego Luis Garavito Henao <diego.garavito@sofka.com.co>
2021-08-13 08:06:44 -0500

    archivos faltantes

---
+++ app/test/FirstTestSpec.js
@@ -0,0 +1,13 @@
+describe("Going", function () {
+    it("verification of value", function () {
+        var a = 12;
+        expect(a).toBe(12);
+    });
+    it("verification of definied", function () {
+        var u;
+        expect(u).toBeDefined("not definied");
+    });
+    it("no verification", function () {
+    });
+});
+//# sourceMappingURL=FirstTestSpec.js.map
---
+++ app/test/FirstTestSpec.ts
@@ -0,0 +1,15 @@
+describe("Going",()=>{
+    it("verification of value",()=>{
+        let a=12;
+        expect(a).toBe(12);
+    });
+
+    it("verification of definied",()=>{
+        let u;
+        expect(u).toBeDefined("not definied");
+    });
+
+    it("no verification",()=>{
+
+    });
+});
